950431b7280a87245e4d3ab9344aff57d804fb97
lam.bui <lam.bui@cloudgo.vn>
2025-04-30 16:54:11 +0700

    Init code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # nodejs-twitter
+### src: https://duthanhduoc.com/blog/setup-du-an-nodejs-typescript
+npm init
+npm i --save-dev @types/express
+npm i nodemon tsx --save-dev
+npm i @types/node --save-dev
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+const app = express()
+const router = express.Router()
+const port = 3000
+
+app.get('/', (req, res) => {
+	res.send('Hello world!')
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
+
+router.use((req, res, next) => {
+	console.log('Time: ', Date.now())
+	next()
+})
+
+router.get('/tweets', (req, res) => {
+	res.json({
+		data: [{ id: 1, text: 'Hello world' }]
+	})
+})
+
+app.use('/', router)
